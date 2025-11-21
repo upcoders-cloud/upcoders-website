fad82e9b8e306b5a986dcaa94f046425e691cdfc
Paweł Biniak <pawix00@gmail.com>
2025-11-21 14:37:45 +0100

    seo add + files clear + og-image

--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -4,24 +4,9 @@ import { OFFER_ITEMS } from "./index.js";
 import Accordion from 'components/Offer/Accordion/Accordion.jsx'
 import { isMobile } from 'react-device-detect'
 
-import ScrollCueHide from 'components/Decor/ScrollCueHide.jsx'
-import ScrollCueReveal from 'components/Decor/ScrollCueReveal.jsx'
-
-import scrollcue from "assets/scrollcue-img.png";
-import scrollreveal from "assets/scrollreveal-img.png";
-
 export default function Offer() {
   return (
     <section id="offer" className="relative bg-bg-2 text-white section-wrapper overflow-hidden">
-      {/*<ScrollCueHide*/}
-      {/*  src={scrollcue}*/}
-      {/*  attachTo="#offer"*/}
-      {/*  className="hidden md:block absolute -top-6 left-0 h-[120px] md:h-[160px] lg:h-[260px] w-auto"*/}
-      {/*  maxTranslateX={60}*/}
-      {/*  maxTranslateY={0}*/}
-      {/*  fadeDistance="40vh"*/}
-      {/*  startOffset={300}*/}
-      {/*/>*/}
       <div className="grid md:grid-cols-2 gap-12 section-inner">
         <div>
           <h3 className="text-xs tracking-widest text-gray-400 mb-2">OFFER</h3>
@@ -33,7 +18,6 @@ export default function Offer() {
             No ready-made solutions, no unnecessary features – only what really
             works and brings value.
           </p>
-          {/* DESKTOP ONLY */}
           {!isMobile && (
             <ZigZag5 size={16} className="opacity-90 mt-16" />
           )}
@@ -47,15 +31,6 @@ export default function Offer() {
           )}
         </div>
       </div>
-      {/*<ScrollCueReveal*/}
-      {/*  src={scrollreveal}*/}
-      {/*  attachTo="#offer"*/}
-      {/*  className="hidden md:block absolute -bottom-6 right-0 h-[120px] md:h-[160px] lg:h-[260px] w-auto"*/}
-      {/*  enterFromX={60}*/}
-      {/*  enterFromY={0}*/}
-      {/*  fadeDistance="40vh"*/}
-      {/*  startOffset={300}*/}
-      {/*/>*/}
     </section>
   );
 }
